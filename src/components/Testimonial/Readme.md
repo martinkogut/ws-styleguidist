935592ad5189bcb361c07d8893b0c7161b5f6928
Martin Kogut <mail@martinkogut.de>
2018-11-15 23:05:48 +0100

    added testimonial

---
+++ src/components/Testimonial/Readme.md
@@ -0,0 +1,6 @@
+```js
+<Testimonial
+  quote="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae"
+  name="Julian Schmitz"
+  company="Google" />
+```
---
+++ src/components/Testimonial/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Avatar from '../Avatar'
+import classNames from 'classnames'
+import s from '../../assets/scss/_testimonials.scss'
+class Testimonial extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { ...props }
+  }
+
+  render() {
+    return (
+      <div id="testimonials" className={s['gray-bg']}>
+        <section className={'container ' + s['testimonials-3col']}>
+          <div className="row section">
+            <div className="col-md-4 mb-sm-50">
+              <div className={s['t-item'] + ' wow fadeIn'} data-wow-duration="1s" style={{visibility: 'visible', animationDuration: '1s', animationName: 'fadeIn'}}>
+              <Avatar src="http://themes.89elements.com/definity-html/assets/images/avatar-1.png" alt="Tony Tester" />
+                <blockquote>
+                  <p>{this.state.quote}</p>
+                  <footer>
+                    <cite>by {this.state.name}<span>{this.state.company}</span></cite>
+                  </footer>
+                </blockquote>
+                <span className={s['et-quote t-icon']}></span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+Testimonial.PropTypes = {
+  quote: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+}
+
+export default Testimonial
